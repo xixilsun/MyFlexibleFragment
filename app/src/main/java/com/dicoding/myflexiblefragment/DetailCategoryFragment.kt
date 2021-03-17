600d42e5b75c15e9e91cd076e1b3e38947e83644
Xixillia Sunaryo <xixilliasun@gmail.com>
2021-03-18 01:30:14 +0700

    Fragment Selesai

--- app/src/main/java/com/dicoding/myflexiblefragment/DetailCategoryFragment.kt
+++ app/src/main/java/com/dicoding/myflexiblefragment/DetailCategoryFragment.kt
@@ -1,5 +1,6 @@
 package com.dicoding.myflexiblefragment
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -39,6 +40,10 @@ class DetailCategoryFragment : Fragment() {
             val mOptionDialogFragment = OptionDialogFragment()
             val mFragmentManager = childFragmentManager
             mOptionDialogFragment.show(mFragmentManager, OptionDialogFragment::class.java.simpleName)
+        }
+        btnProfile.setOnClickListener{
+            val mIntent = Intent(activity, ProfileActivity::class.java)
+            startActivity(mIntent)
         }
     }
 
